9a13f0755b7a7cb0ce470662d5ab8e1886de4687
liyongbin <2637230700@qq.com>
2025-04-11 11:56:12 +0800

    init

---
+++ src/main/java/com/haust/easyremotemcp/EasyRemoteMcpApplication.java
@@ -0,0 +1,13 @@
+package com.haust.easyremotemcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyRemoteMcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyRemoteMcpApplication.class, args);
+    }
+
+}
